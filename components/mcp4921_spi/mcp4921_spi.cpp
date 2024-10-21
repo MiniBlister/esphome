3b28d209914a2495e4192ca26419a93c89d8a9ac
MiniBlister <inet@kohrs.biz>
2024-10-21 15:56:13 +0200

    New

--- components/mcp4921_spi/mcp4921_spi.cpp
+++ components/mcp4921_spi/mcp4921_spi.cpp
@@ -10,19 +10,30 @@ void MCP4921SPI::setup() {
     ESP_LOGI(TAG, "MCP4921SPI setup started!");
     this->spi_setup();
     ESP_LOGI(TAG, "SPI setup finished!");
+    this->set_traits();
 }
 
-void MCP4921SPI::update() {
-
-}
-
-void MCP4921SPI::loop() {
-
+void MCP4921SPI::write_value(float value) {
+  // Code, um den Wert an den MCP4921 DAC über SPI zu senden
 }
 
 void MCP4921SPI::dump_config(){
     ESP_LOGCONFIG(TAG, "Empty SPI sensor");
 }
+
+void MCP4921SPI::on_value(float value) {
+  write_value(value);  // Schreibe den Wert an den DAC
+}
+
+void MCP4921SPI::set_traits() {
+  this->traits.set_min_value(0.0f);
+  this->traits.set_max_value(4095.0f);
+  this->traits.set_step(1.0f);
+  this->traits.set_mode(number::NUMBER_MODE_SLIDER);  // Setze den Modus
+  this->traits.set_unit_of_measurement("V");  // Setze die Einheit
+  this->traits.set_device_class("voltage");  // Setze die Geräteklasse
+}
+
 
 }  // namespace mcp4921_spi
 }  // namespace esphome
--- components/mcp4921_spi/mcp4921_spi.h
+++ components/mcp4921_spi/mcp4921_spi.h
@@ -12,26 +12,15 @@ class MCP4921SPI : public sensor::Sensor,
                        public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING,
                                             spi::DATA_RATE_1KHZ> {
  public:
-  void setup() override;
-  void update() override;
-  void loop() override;
+  void setup() override;  // Nur Deklaration hier
+  void write_value(float value);
   void dump_config() override;
 
-  protected:
-  // Traits for the number component
-  void set_traits() {
-    this->traits.set_min_value(0.0f);
-    this->traits.set_max_value(4095.0f);
-    this->traits.set_step(1.0f);
-    this->traits.set_mode(number::NUMBER_MODE_SLIDER);  // Set default mode
-    this->traits.set_unit_of_measurement("V");  // Set unit
-    this->traits.set_device_class("voltage");  // Set device class
-  }
+ protected:
+  void on_value(float value) override;
 
-  void on_enable() override {
-    this->set_traits();  // Set traits when enabled
-    
-  }
+ private:
+  void set_traits();  // Nur Deklaration hier
 
 };
 
